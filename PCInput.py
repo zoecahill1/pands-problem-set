3d238f48aa1677ec473a5783b2804b4b97dcdad8
Zoe <zcahill90@gmail.com>
2019-02-27 15:50:55 +0000

    Added getfloat.py to pcinput.py

--- PCInput.py
+++ PCInput.py
@@ -1,8 +1,10 @@
 #  Zoe Cahill - Contains functions which handle user input
 # Adapted from Coder's Apprentice Appendix C
 
-# Defines the function
+# Defines the function - This will check users has inputted positive numbers only
 def getPosInt(x):
+    # Will keep looping until we meet condition
+    # Used as we do not know how many tries user will need to input correct data
     while True:
         try:
             # Testing to see if number is an int
@@ -23,9 +25,26 @@ def getPosInt(x):
     # Returns the number if it passes above checks to main program
     return num
 
-# Defines the function
+# Defines the function - This function will take a user inputted string and return it as a list of words
 def getSplitString(x):
     # This line gets the users input
     line = input(x)
     # This returns the string seperated by white spaces as a list ie "Hello world" becomes ["Hello", "world"]
     return line.split()
+
+# Defines the function - This function will check user has inputted a positive number only 
+# and will make sure it is returning float type
+def getFloat(x):
+    # Will keep looping until we meet condition
+    # Used to keep asking for user input until condition is met
+    while True:
+        # Testing to see if input is number - if int will make into float
+        try:
+            num = float(input(x))
+        # Will catch error if user inputs something that is not a number
+        except ValueError:
+            print( "That is not a number -- please try again" )
+            # Returns control to beginning of while loop to check next input
+            continue
+        # When checks are passed returns float to program
+        return num
