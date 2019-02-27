f2dbed12c44a350df789b4d3d0ef6d6551e2b174
Zoe <zcahill90@gmail.com>
2019-02-27 17:55:16 +0000

    Removed for loop from sumupto.py making it more effiient

--- sumupto.py
+++ sumupto.py
@@ -1,22 +1,24 @@
 # Zoe Cahill -  Question 1 Solution
 
-# Imports a function from PCInput which was created to handle user input
-from PCInput import getPosInt 
+def sum():
+    # Imports a function from PCInput which was created to handle user input
+    from PCInput import getPosInt 
 
-# Describes the program to the user
-print("This program will output the sum of all the numbers between 1 and whatever you enter into the next line!")
+    # Describes the program to the user
+    print("This program will output the sum of all the numbers between 1 whatever you choose")
 
-# Calls the function imported above
-# This function will check that the number entered is a positive integer only
-num = getPosInt("Please enter a positive number: ")
+    # Calls the function imported above
+    # This function will check that the number entered is a positive integer only
+    num = getPosInt("Please enter a positive number: ")
 
-# Initializes the count at 0
-ans = 0 
+    # Made code more efficient by removing for loop
+    # Adapted from stack overflow see below for original loop
+    sum = ((num * (num+1))/2)
 
-# Uses for loop and range function to loop from 1 to user inputted value
-for i in range(1, num+1):
-    # Increases count by 1 
-    ans = ans + i
+    #for i in range(1, num+1):
+    #sum = sum + i
 
-# Print out the sum of all the numbers
-print (ans)
+    # Print out the sum of all the numbers
+    print (int(sum))
+
+sum()
