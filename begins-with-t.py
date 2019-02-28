8b59dcef1c4ee7d631671e6d2a865206af5f63ce
Zoe <zcahill90@gmail.com>
2019-02-28 22:21:29 +0000

    Added comments to begins-with-t.py

--- begins-with-t.py
+++ begins-with-t.py
@@ -1,18 +1,21 @@
 # Zoe Cahill - Question 2 Solution
 
-# Need to import this as we will be using some functions within datetime for our program
-import datetime
+def beginsWithT():
+    # Need to import this as we will be using some functions within datetime for our program
+    import datetime
 
-# Describes the program to the user
-print ("This program will check if today begins with a \"T\" or not")
+    # Describes the program to the user
+    print ("This program will check if today begins with a \"T\" or not")
 
-# Checks what todays date is and assigns it to variable x
-day = datetime.datetime.now() 
-# Formats that date to the weekday full version
-day = day.strftime("%A") 
+    # Checks what todays date is and assigns it to variable x
+    day = datetime.datetime.now() 
+    # Formats that date to the weekday full version
+    day = day.strftime("%A") 
 
-# Checks if the first letter of the string is T or not
-if day[0] == "T": 
-    print ("Today is" , day , "so it does begin with a \"T\"!")
-else: 
-    print ("Today is", day , "so it does not begin with a \"T\"!")
+    # Checks if the first letter of the string is T or not
+    if day[0] == "T": 
+        print ("Today is" , day , "so it does begin with a \"T\"!")
+    else: 
+        print ("Today is", day , "so it does not begin with a \"T\"!")
+
+beginsWithT()
